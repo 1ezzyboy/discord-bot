8dd7e7772f68f7455234c5319c3f164b4d497725
Ezra <ezra.furry@gmail.com>
2020-11-11 16:09:30 -0500

    added spaces for code readability

--- ezzy_bot.py
+++ ezzy_bot.py
@@ -5,8 +5,10 @@ from dotenv import load_dotenv
 from discord.ext import commands
 
 load_dotenv()
+
 token = os.getenv("DISCORD_TOKEN")
 guild = os.getenv("DISCORD_GUILD")
+
 bot = commands.Bot(command_prefix='!')
 client = discord.Client()
 
